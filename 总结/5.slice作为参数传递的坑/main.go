483b56c53637c8562cfa7679a85fbb9a79d7d1f9
fq <1078649116@qq.com>
2020-03-16 17:08:13 +0800

    add：slice总结

--- 总结/5.slice作为参数传递的坑/main.go
+++ 总结/5.slice作为参数传递的坑/main.go
@@ -10,8 +10,13 @@ func changeSliceDataCapError(data []int){
 	data = append(data,10)
 }
 
+//传递指针
+func changeDataByPoint(data *[]int) {
+  *data = append(*data,10)
+}
+
 func main() {
-	data := make([]int,20,20)
+	data := make([]int,10,20)
 	for i := 0;i < 10 ; i ++ {
 		data[i] = i
 	}
@@ -34,6 +39,10 @@ func main() {
 	//append这个函数则根据情况不同有两种工作方式：
 	// 若你slice的cap足够，则直接修改其引用的数组区域并简单将len增加，并返回这个slice本身，即这时候h=append(h,...)之后，h还是引用原来的数组区域，只不过h的len增加了；
 	// 若cap不够，则重新申请一块数组区域并将原来的数组内容拷贝过去后再进行追加元素操作，这时候append的返回的slice引用的是另一块内存了
+	fmt.Println(data)
 
+	//任何时候传指针是可以的
+	//https://www.cnblogs.com/snowInPluto/p/7477365.html
+	changeDataByPoint(&data)
 	fmt.Println(data)
 }
