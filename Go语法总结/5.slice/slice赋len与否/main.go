1cc62d2feafe2160fd518e09dc721562f52386af
Crshi <la9235961@foxmail.com>
2020-06-08 10:14:59 +0800

    add:slice test

--- Go语法总结/5.slice/slice赋len与否/main.go
+++ Go语法总结/5.slice/slice赋len与否/main.go
@@ -13,7 +13,7 @@ type test struct {
 func main() {
 
 	result1 := make([]*test, 1000000)
-	result2 := make([]*test, 1000000)
+	result2 := []*test{}
 
 	start1 := time.Now()
 
@@ -23,14 +23,19 @@ func main() {
 			Id:   i,
 			name: "name" + string(i),
 		}
-
-		result2[i] = &test{
-			Id:   i,
-			name: "name" + string(i),
-		}
 	}
 
-	for k,v := range 
-
 	fmt.Println(time.Since(start1).Seconds())
+
+	start2 := time.Now()
+
+	//0.391
+	for i := 0; i < 1000000; i++ {
+		result2 = append(result2, &test{
+			Id:   i,
+			name: "name" + string(i),
+		})
+	}
+
+	fmt.Println(time.Since(start2).Seconds())
 }
